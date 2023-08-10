9957c53c9f282cf3a51c144e34317e24f2a58191
Dennis <dennis@Zettas-MacBook-Pro.local>
2023-08-10 17:27:02 +0300

    Routing/

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,5 @@
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.scss'
 import { Box } from '~/uikit'
 import { Routes, Route, Link } from 'react-router-dom'
 
